6a0254ad635a4dff55ecf3f256be03cca907fe7a
KlfJoat <klfjoat@server.fake>
2013-11-28 22:52:11 -0600

    Add install.sh to master

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# install.sh
+#
+# Install current script to bin
+#
+
+files = "ripdvd"
+
+install_to = "/home/klfjoat/bin"
+
+for i in "$files"; do
+  cp "$i" "$install_to"
+done
+
