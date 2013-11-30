a6abc7f1cff52d2b47e9d3c7bf126c24ffcda0bd
KlfJoat <klfjoat@server.fake>
2013-11-29 19:23:04 -0600

    Can't have spaces before assignment operators in bash--fixed

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 # Install current script to bin
 #
 
-files = "ripdvd"
+files="ripdvd"
 
-install_to = "/home/klfjoat/bin"
+install_to="/home/klfjoat/bin"
 
 for i in "$files"; do
   cp "$i" "$install_to"
